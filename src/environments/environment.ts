82dea5235d7b163d2509a0adf2589398ee2794d9
rodrigo mendez <rodrigo_2392@hotmail.com>
2019-12-19 19:15:04 -0600

    hide google vars

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,6 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: "AIzaSyCIQJbD1X2i6orwf0yUmrmbYRk4JlKixG8",
-    authDomain: "angular-auth-fcc51.firebaseapp.com",
-    databaseURL: "https://angular-auth-fcc51.firebaseio.com",
-    projectId: "angular-auth-fcc51",
-    storageBucket: "angular-auth-fcc51.appspot.com",
-    messagingSenderId: "196470140935",
-    appId: "1:196470140935:web:7aef60dbd338b6071fbaa2"
-  }
 };
 
 /*
